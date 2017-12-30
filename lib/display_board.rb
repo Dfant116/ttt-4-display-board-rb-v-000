3d5f8271507a1a9ac7e3ce5956165a3773129b85
Dalton Fant <daltonfant16@gmail.com>
2017-12-30 03:13:42 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,9 @@
 def display_board(board)
-  puts "   |   |   ",
+  puts "#{board[0]}|#{board[1]}|#{board[2]}",
        "-----------",
-       "   |   |   ",
+       "#{board[3]}|#{board[4]}|#{board[5]}",
        "-----------",
-       "   |   |   "
+       "#{board[6]}|#{board[7]}|#{board[8]}"
      end
 
 puts "Turn 1"
