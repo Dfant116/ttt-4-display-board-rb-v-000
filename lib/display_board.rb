1a81e8b68dbc50bc022bb4c1decc650c16e8f8da
Dalton Fant <daltonfant16@gmail.com>
2017-12-30 03:11:15 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -9,4 +9,7 @@ def display_board(board)
 puts "Turn 1"
 board = ["   ", "   ", "   ", "   ", "   ", "   ", "   ", "   ", "   "]
   display_board(board)
-  
+
+puts "Turn 2"
+board = ["   ", "   ", "   ", "   ", " X ", "   ", "   ", "   ", "   "]
+display_board(board)
